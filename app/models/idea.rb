ddc2b93078a4a9d27c2568cadea5a3e753037521
David Stinnette <dastinnette@gmail.com>
2016-03-21 17:06:18 -0600

    Idea model tests passing

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,5 @@
 class Idea < ActiveRecord::Base
+  validates :title, presence: true,
+                    uniqueness: true
+  validates :body, presence: true
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is invalid without a title" do
+    idea = Idea.create(body: "Pigeons should never be trusted")
+
+    expect(idea).to be_invalid
+  end
+
+  it "is invalid without a body" do
+    idea = Idea.create(title: "Invest in Blockbuster")
+
+    expect(idea).to be_invalid
+  end
+
+  it "has a default value of 0" do
+    idea = Idea.create(title: "Wear more suspenders", body: "All day, erry day")
+
+    expect(idea.quality).to eq(0)
+  end
+
 end
