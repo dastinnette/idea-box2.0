f87ba9107b8d7bb3004fb680e46aaefa989dc6bc
David Stinnette <dastinnette@gmail.com>
2016-03-23 17:32:15 -0600

    Refactor idea update functions

--- app/assets/javascripts/idea_update.js
+++ app/assets/javascripts/idea_update.js
@@ -2,17 +2,17 @@ function updateIdeaTitle(){
   $('#ideas').delegate('.idea-title', 'keydown', function(event){
     var dispatchAjax = event.which === 13
     if (dispatchAjax) {
-        event.preventDefault()
-        var $idea = $(this).closest('.idea')
-        var newTitle = { title: this.textContent }
+      event.preventDefault()
+      var $idea = $(this).closest('.idea')
+      var newTitle = { title: this.textContent }
 
-        this.blur()
+      this.blur()
 
-        $.ajax({
-          type: "PUT",
-          url: 'api/v1/ideas/' + $idea.attr('data-id'),
-          data: newTitle
-        })
+      $.ajax({
+        type: "PUT",
+        url: 'api/v1/ideas/' + $idea.attr('data-id'),
+        data: newTitle
+      })
     }
   })
 }
@@ -21,17 +21,17 @@ function updateIdeaBody(){
   $('#ideas').delegate('.idea-body', 'keydown', function(event){
     var dispatchAjax = event.which === 13
     if (dispatchAjax) {
-        event.preventDefault()
-        var $idea = $(this).closest('.idea')
-        var newBody = { body: this.textContent }
+      event.preventDefault()
+      var $idea = $(this).closest('.idea')
+      var newBody = { body: this.textContent }
 
-        this.blur()
+      this.blur()
 
-        $.ajax({
-          type: "PUT",
-          url: 'api/v1/ideas/' + $idea.attr('data-id'),
-          data: newBody
-        })
+      $.ajax({
+        type: "PUT",
+        url: 'api/v1/ideas/' + $idea.attr('data-id'),
+        data: newBody
+      })
     }
   })
 }
--- app/assets/javascripts/idea_update_quality.js
+++ app/assets/javascripts/idea_update_quality.js
@@ -2,48 +2,46 @@ var increaseQuality = {
   swill: 'plausible',
   plausible: 'genius',
   genius: 'genius'
-};
+}
 
 var decreaseQuality = {
   swill: 'swill',
   plausible: 'swill',
   genius: 'plausible'
-};
+}
 
 function likeIdea(){
   $('#ideas').delegate('.like-idea', 'click', function(){
-    var idea    = $(this).closest('.idea');
-    var ideaId  = idea.attr('data-id');
-    var quality = idea.find('.idea-quality');
-    var qualityText = quality.text();
-    var data = { quality: increaseQuality[qualityText] };
+    var $idea   = $(this).closest('.idea')
+    var quality = idea.find('.idea-quality')
+    var qualityText = quality.text()
+    var data = { quality: increaseQuality[qualityText] }
 
     $.ajax({
       type: 'PUT',
-      url: '/api/v1/ideas/' + ideaId,
+      url: '/api/v1/ideas/' + $idea.attr('data-id'),
       data: data,
       success: function(){
-        quality.text(increaseQuality[qualityText]);
+        quality.text(increaseQuality[qualityText])
       }
     })
   })
-};
+}
 
 function dislikeIdea(){
   $('#ideas').delegate('.dislike-idea', 'click', function(){
-    var idea    = $(this).closest('.idea');
-    var ideaId  = idea.attr('data-id');
-    var quality = idea.find('.idea-quality');
-    var qualityText = quality.text();
-    var data = { quality: decreaseQuality[qualityText] };
+    var $idea   = $(this).closest('.idea')
+    var quality = idea.find('.idea-quality')
+    var qualityText = quality.text()
+    var data = { quality: decreaseQuality[qualityText] }
 
     $.ajax({
       type: 'PUT',
-      url: '/api/v1/ideas/' + ideaId,
+      url: '/api/v1/ideas/' + $idea.attr('data-id'),
       data: data,
       success: function(){
-        quality.text(decreaseQuality[qualityText]);
+        quality.text(decreaseQuality[qualityText])
       }
     })
   })
-};
+}
