876fc6955940eebdd9076d7dcd2277b164edba9a
David Stinnette <dastinnette@gmail.com>
2016-03-22 18:46:43 -0600

    Created new idea form

---
+++ app/assets/javascripts/idea_post.js
@@ -0,0 +1,15 @@
+function postIdea(){
+// we need a two text fields one for title one for boduy
+// we need a save button
+
+// we need an event listener for the save button
+// when that button is clicked we will...
+
+// ajax to the create end point in our internal api.
+// in order for this ajax call to be succesful you will need to grab the values
+// within the input fields and send it over as a data.
+// creating a javascript object with the values of the input field
+// var postParams ={ name: values in the field, body: values in body field}
+// data: postParams
+
+}
