c7e0a57cec982aeea21963068f3105c76a39409a
David Stinnette <dastinnette@gmail.com>
2016-03-22 19:11:56 -0600

    Sucessfully clearing input fields in real time

--- app/assets/javascripts/idea_post.js
+++ app/assets/javascripts/idea_post.js
@@ -9,11 +9,16 @@ function postIdea(){
       data: ideaPost,
       success: function(ideas) {
         renderIdea(ideas)
+        clearInputField()
       }
     })
 
   })
 
+function clearInputField(){
+  $('#title').val("")
+  $('#body').val("")
+}
 // we also need to clear the input and out put fields
 
 }
