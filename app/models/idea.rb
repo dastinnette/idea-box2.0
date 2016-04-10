7ee2f1eea56bc8fc26356bbc8f5499a6c90bac97
David Stinnette <dastinnette@gmail.com>
2016-04-10 08:42:32 -0600

    Update idea.rb

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,5 @@
 class Idea < ActiveRecord::Base
-  validates :title, format: { with: /\A[a-zA-Z0-9,?! ]+\z/ }
-  validates :body, format: { with: /\A[a-zA-Z0-9,?! ]+\z/ }
+  validates :title, format: { with: /\A[a-zA-Z0-9.,?! ]+\z/ }
+  validates :body, format: { with: /\A[a-zA-Z0-9.,?! ]+\z/ }
   enum quality: [:swill, :plausible, :genius]
 end
